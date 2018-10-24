236f4e7485dc4f87ee35ba93d13bb78531511d95
DarkDefiledDanny <dheijnen123@gmail.com>
2018-10-24 22:45:30 +0200

    Resolved issue with utils import path

--- src/Handlers/Uploader/routes.js
+++ src/Handlers/Uploader/routes.js
@@ -1,9 +1,9 @@
 /* @flow */
 import Boom from 'boom';
-import path from 'path'
+import path from 'path';
 import fs from 'fs';
 import Loki from 'lokijs';
-import { imageFilter, loadCollection, uploader } from './utils';
+import { imageFilter, loadCollection, uploader } from '../../utils';
 
 //Uploader SETUP
 
@@ -15,93 +15,97 @@ const DB = new Loki(`${UPLOAD_PATH}/${DB_NAME}`, { persistenceMethod: 'fs' });
 
 if (!fs.existsSync(UPLOAD_PATH)) fs.mkdirSync(UPLOAD_PATH);
 
-
-
-
 const routes = [
-{
+  {
     method: 'POST',
     path: '/Uploader/SingleUpload',
     config: {
       auth: false,
       tags: ['api'],
-        payload: {
-            output: 'stream',
-            allow: 'multipart/form-data' // important
-        }
+      payload: {
+        output: 'stream',
+        allow: 'multipart/form-data' // important
+      }
     },
-    handler: async function (request, reply) {
-        try {
-            const data = request.payload;
-            const file = data['avatar']; // accept a field call avatar
+    handler: async function(request, reply) {
+      try {
+        const data = request.payload;
+        const file = data['avatar']; // accept a field call avatar
 
-            // save the file
-            const fileDetails = await uploader(file, fileOptions);
+        // save the file
+        const fileDetails = await uploader(file, fileOptions);
 
-            // save data to database
-            const col = await loadCollection(COLLECTION_NAME, DB);
-            const result = col.insert(fileDetails);
-            DB.saveDatabase();
+        // save data to database
+        const col = await loadCollection(COLLECTION_NAME, DB);
+        const result = col.insert(fileDetails);
+        DB.saveDatabase();
 
-            // return result
-            return({ id: result.$loki, fileName: result.filename, originalName: result.originalname });
-
-        } catch (err) {
-            // error handling
-            return(Boom.badRequest(err.message, err));
-        }
+        // return result
+        return {
+          id: result.$loki,
+          fileName: result.filename,
+          originalName: result.originalname
+        };
+      } catch (err) {
+        // error handling
+        return Boom.badRequest(err.message, err);
+      }
     }
-},
-{
+  },
+  {
     method: 'POST',
     path: '/Uploader/MultipleUpload',
     config: {
       auth: false,
       tags: ['api'],
-        payload: {
-            output: 'stream',
-            allow: 'multipart/form-data'
-        }
+      payload: {
+        output: 'stream',
+        allow: 'multipart/form-data'
+      }
     },
-    handler: async function (request, reply) {
-        try {
-            const data = request.payload;
-            const files = data['photos'];
+    handler: async function(request, reply) {
+      try {
+        const data = request.payload;
+        const files = data['photos'];
 
-            const filesDetails = await uploader(files, fileOptions);
-            const col = await loadCollection(COLLECTION_NAME, DB);
-            const result = [].concat(col.insert(filesDetails));
+        const filesDetails = await uploader(files, fileOptions);
+        const col = await loadCollection(COLLECTION_NAME, DB);
+        const result = [].concat(col.insert(filesDetails));
 
-            DB.saveDatabase();
-            return(result.map(x => ({ id: x.$loki, fileName: x.filename, originalName: x.originalname })));
-        } catch (err) {
-            return(Boom.badRequest(err.message, err));
-        }
+        DB.saveDatabase();
+        return result.map(x => ({
+          id: x.$loki,
+          fileName: x.filename,
+          originalName: x.originalname
+        }));
+      } catch (err) {
+        return Boom.badRequest(err.message, err);
+      }
     }
-},
-{
+  },
+  {
     method: 'GET',
     path: '/Uploader/images/{id}',
     config: {
       auth: false,
-      tags:['api']
+      tags: ['api']
     },
-    handler: async function (request, reply) {
-        try {
-            const col = await loadCollection(COLLECTION_NAME, DB)
-            const result = col.get(request.params['id']);
+    handler: async function(request, reply) {
+      try {
+        const col = await loadCollection(COLLECTION_NAME, DB);
+        const result = col.get(request.params['id']);
 
-            if (!result) {
-                return(Boom.notFound());
-                return;
-            };
-
-            return(fs.createReadStream(path.join(UPLOAD_PATH, result.filename)))
-             
-        } catch (err) {
-            return(Boom.badRequest(err.message, err));
+        if (!result) {
+          return Boom.notFound();
+          return;
         }
+
+        return fs.createReadStream(path.join(UPLOAD_PATH, result.filename));
+      } catch (err) {
+        return Boom.badRequest(err.message, err);
+      }
     }
-}];
+  }
+];
 
 export default routes;
--- src/index.js
+++ src/index.js
@@ -11,24 +11,6 @@ import Bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import { Auth } from './Models';
 import { getKey } from './Services';
-import Boom from 'boom';
-import path from 'path'
-import fs from 'fs';
-import Loki from 'lokijs';
-import { imageFilter, loadCollection, uploader } from './utils';
-
-//Uploader SETUP
-
-const DB_NAME = 'DB.json';
-const COLLECTION_NAME = 'images';
-const UPLOAD_PATH = 'uploads';
-const fileOptions: FileUploaderOption = { dest: `${UPLOAD_PATH}/` };
-const DB = new Loki(`${UPLOAD_PATH}/${DB_NAME}`, { persistenceMethod: 'fs' });
-
-if (!fs.existsSync(UPLOAD_PATH)) fs.mkdirSync(UPLOAD_PATH);
-
-
-
 
 consoleTime(console, {
   pattern: 'dd/mm/yyyy HH:MM:ss.l',
@@ -44,94 +26,6 @@ if (process.env.NODE_ENV !== 'production') {
 const port = process.env.PORT || 3000;
 
 const server = new Hapi.server({ port });
-
-server.route({
-    method: 'POST',
-    path: '/SingleUpload',
-    config: {
-      auth: false,
-      tags: ['api'],
-        payload: {
-            output: 'stream',
-            allow: 'multipart/form-data' // important
-        }
-    },
-    handler: async function (request, reply) {
-        try {
-            const data = request.payload;
-            const file = data['avatar']; // accept a field call avatar
-
-            // save the file
-            const fileDetails = await uploader(file, fileOptions);
-
-            // save data to database
-            const col = await loadCollection(COLLECTION_NAME, DB);
-            const result = col.insert(fileDetails);
-            DB.saveDatabase();
-
-            // return result
-            return({ id: result.$loki, fileName: result.filename, originalName: result.originalname });
-
-        } catch (err) {
-            // error handling
-            return(Boom.badRequest(err.message, err));
-        }
-    }
-});
-
-server.route({
-    method: 'POST',
-    path: '/MultipleUpload',
-    config: {
-      auth: false,
-      tags: ['api'],
-        payload: {
-            output: 'stream',
-            allow: 'multipart/form-data'
-        }
-    },
-    handler: async function (request, reply) {
-        try {
-            const data = request.payload;
-            const files = data['photos'];
-
-            const filesDetails = await uploader(files, fileOptions);
-            const col = await loadCollection(COLLECTION_NAME, DB);
-            const result = [].concat(col.insert(filesDetails));
-
-            DB.saveDatabase();
-            return(result.map(x => ({ id: x.$loki, fileName: x.filename, originalName: x.originalname })));
-        } catch (err) {
-            return(Boom.badRequest(err.message, err));
-        }
-    }
-});
-
-server.route({
-    method: 'GET',
-    path: '/images/{id}',
-    config: {
-      auth: false,
-      tags:['api']
-    },
-    handler: async function (request, reply) {
-        try {
-            const col = await loadCollection(COLLECTION_NAME, DB)
-            const result = col.get(request.params['id']);
-
-            if (!result) {
-                return(Boom.notFound());
-                return;
-            };
-
-            return(fs.createReadStream(path.join(UPLOAD_PATH, result.filename)))
-               // .header('Content-Type', result.mimetype); // important
-        } catch (err) {
-            return(Boom.badRequest(err.message, err));
-        }
-    }
-});
-
 
 /*
 server.route({
